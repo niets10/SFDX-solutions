deca746b3844b7a441d4950801d2a76f7eaf05c3
Álvaro Nieto <agnieto00@gmail.com>
2021-05-14 16:04:36 +0200

    iframe in lwc

---
+++ force-app/code/lwc-iframe/lwc/iframeCmp/iframeCmp.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class IframeCmp extends LightningElement {
+
+    get fullUrl() {
+
+        return 'https://www.marca.com/';
+        }
+
+}
